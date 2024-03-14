afee3bfed065c19f67214cce3c41b6296db74a39
Alvi Dediev <alvi.dediev@autovill.ru>
2024-03-14 22:07:20 +0300

    Initial commit

---
+++ src/Lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+package Lesson2;
+
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+        byte b3 = 100;
+
+        short s1 = 5;
+        short s2 = -10;
+        short s3 = 0;
+
+        int i1 = 500;
+
+        long l1 = 1000;
+        long l2 = 10000000000L;
+        long l3 = 51L;
+
+        float f1 = 3.14F;
+        float f2 = 2.5f;
+        float f3 = 20.0f;
+
+        double d1 = 5.5;
+        double d2 = 87.65;
+
+        char c1 = 'a';
+        char c2 = 'A';
+    }
+}
